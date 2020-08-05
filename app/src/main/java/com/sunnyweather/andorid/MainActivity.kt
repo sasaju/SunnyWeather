e325e89288957faa2161aa73c162fb527a3175a7
sasaju <1289142675@qq.com>
2020-08-05 21:31:46 +0800

    First commit

---
+++ app/src/main/java/com/sunnyweather/andorid/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.sunnyweather.andorid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
